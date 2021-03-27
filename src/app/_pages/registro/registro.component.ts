00f2a1193cdc27f7161a15b3f119dbcbd1008235
Julio-Usac <3300473961201@ingenieria.usac.edu.gt>
2021-03-26 23:00:10 -0600

    Se agrego funciones al registro de usuarios

--- src/app/_pages/registro/registro.component.ts
+++ src/app/_pages/registro/registro.component.ts
@@ -17,14 +17,16 @@ export class RegistroComponent implements OnInit {
     this.data = this.db.list('/RegistroUsuario');
    }
 
-  nombreUsuario;
-  apellidoUsuario;
-  emailUsuario;
+  nombreUsuario: string='';
+  apellidoUsuario: string='';
+  emailUsuario: string='';
   generoUsuario;
   ciudadUsuario;
-
+  contrasenaUsuario: string='';
+  vercontrasena: string='';
   dataUsuario = [];
   ngOnInit(): void {
+    
     this.data.snapshotChanges().subscribe(d => {
       this.dataUsuario = [];
       d.forEach(item => {
@@ -34,25 +36,58 @@ export class RegistroComponent implements OnInit {
           apellido: item.payload.val().apellido,
           email: item.payload.val().email,
           genero: item.payload.val().genero,
-          ciudad: item.payload.val().ciudad
+          ciudad: item.payload.val().ciudad,
+          contrasena: item.payload.val().contrasena,
         }
         this.dataUsuario.push(RegistroUsuario);
       })
     })
   }
   VerificacionEdad( a:number ): number{
-    return 2021-a
+    return 2021-a;
+  }
+  VerificacionCampos(a:string): boolean{
+    if (a.length>0){
+      return true;
+    }
+    return false;
+  }
+  CompararContraseña(a:string, b:string): boolean{
+    if(a==b){
+      return true;
+    }
+    return false;
+  }
+  SeguridadContraseña(a:string): string{
+    if (a.length>3){
+      return "seguro"
+    }
+    return "inseguro"
   }
   registrar(){
-    this.edad =this.VerificacionEdad(Number(this.fecha.substring(0,4)));
-    let RegistroUsuario= {
-      nombre: this.nombreUsuario,
-      apellido: this.apellidoUsuario,
-      email: this.emailUsuario,
-      genero: this.generoUsuario,
-      ciudad: this.ciudadUsuario
-    }    
-    this.data.push(RegistroUsuario);
+    
+    if(this.VerificacionCampos(this.nombreUsuario) && this.VerificacionCampos(this.apellidoUsuario) && this.VerificacionCampos(this.emailUsuario) && this.VerificacionCampos(this.contrasenaUsuario)){
+      if (this.VerificacionEdad(this.edad)>17){
+        this.edad =this.VerificacionEdad(Number(this.fecha.substring(0,4)));
+        if(this.contrasenaUsuario==this.vercontrasena){
+          let RegistroUsuario= {
+            nombre: this.nombreUsuario,
+            apellido: this.apellidoUsuario,
+            email: this.emailUsuario,
+            genero: this.generoUsuario,
+            ciudad: this.ciudadUsuario,
+            contrasena: this.contrasenaUsuario
+          }    
+          this.data.push(RegistroUsuario);
+          alert("Usuario Registrado")
+        }else{
+          alert("Las contraseñas no coinciden");
+        }
+      }else{
+        alert("No puedes crear una cuenta si eres menor de edad")
+      }
+    }else{
+      alert("Hay campos en blanco");
+    }
   }
 }
-
