fec6fe37123efdfaa1cc9465a6f421da099d8986
AlisonLeiva <alisonleiva24@gmail.com>
2021-03-26 23:25:38 -0600

    Se muestran en la tabla los usuarios de la base de datos

--- src/app/model/user.ts
+++ src/app/model/user.ts
@@ -2,8 +2,8 @@ export interface User {
     id:number,
     name: string,
     lastname: string,
-    username: string,
     email: string,
     gender: string,
     city: string,
+    password:string
 }
