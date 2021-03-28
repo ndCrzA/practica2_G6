a1aca1804fbd8da50b4b1ac793e489da9b42ebd3
NELSON <CRUZ>
2021-03-27 21:34:20 -0600

    se agrega configuracion firebase para los archivos spect

--- src/app/user-list/user-list.component.spec.ts
+++ src/app/user-list/user-list.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from 'src/environments/environment';
 
 import { UserListComponent } from './user-list.component';
 
@@ -8,7 +10,10 @@ describe('UserListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UserListComponent ]
+      declarations: [ UserListComponent ],
+      imports:[
+        AngularFireModule.initializeApp(environment.firebase)        
+      ],
     })
     .compileComponents();
   });
